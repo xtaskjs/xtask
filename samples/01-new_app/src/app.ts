1d80cf2666e1f13a00ea9ed51daf506fa07a9763
Xavi <email@javier-rodriguez.com>
2025-12-27 22:34:26 +0100

    Chore: Added samples folder

--- samples/01-new_app/src/app.ts
+++ samples/01-new_app/src/app.ts
@@ -1,7 +1,7 @@
-import {Bootstrap} from "@xtaskjs/core";
+import * as xtask from "@xtaskjs/core";
 
 async function main() {
-    const kernel = await Bootstrap();}
+    const kernel = await xtask.Bootstrap();}
 main().catch(err => {
     console.error("Error starting the application:", err);
 }); 
